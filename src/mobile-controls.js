7f40bb2ba6db9c1fa1461d9afec4dc1104c0b661
michaelfiber <michael+michaelfiber@cubeofb.org>
2022-11-27 12:07:41 -0500

    Update minshell styling and start mobile controls implementation

--- src/mobile-controls.js
+++ src/mobile-controls.js
@@ -1,3 +1,22 @@
+var leftHolder;
+var rightHolder;
+
+function buildDpad() {
+	var bUp = document.createElement('button');
+	bUp.innerText = '^';
+	bUp.style.width = "33%";
+	bUp.style.left = "33%";
+	leftHolder.appendChild(bUp);
+}
+
+function init() {
+	leftHolder = document.querySelector("#left-control-holder");
+	rightHolder = document.querySelector("#right-control-holder");
+
+	buildDpad();
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-	console.log("Mobile controls online.");
+	init();
 });
+
