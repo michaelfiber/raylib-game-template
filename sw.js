e2d8aa9bc5329f3fb9ff132a47a8754eab78b630
Action: Push to Branch <action@github.com>
2022-11-28 02:42:31 +0000

    Update gh-pages to output generated at

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = "cache_name_1669603350";
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(cacheName));
+});
+
+
+// Cache all requests. This is why the PWA option should be set to false during dev.
+self.addEventListener('fetch', async (event) => {
+	let cache = await caches.open(cacheName);
+	let response = await cache.match(event.request);
+
+	if (!response) {
+		response = await fetch(event.request.url);
+		cache.put(event.request, response.clone());
+	}
+
+	event.respondWith(response);
+});
