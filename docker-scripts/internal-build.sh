d0f46dd78820c6cc45f23e1c08124b5e991e3a9f
Michael <michael+michaelfiber@cubeofb.org>
2023-10-26 10:59:20 -0400

    Remove logging

--- docker-scripts/internal-build.sh
+++ docker-scripts/internal-build.sh
@@ -19,8 +19,6 @@ mkdir -p /app/project/dist
 
 mv index.* /app/project/dist/
 
-ls -al /app/project/dist/
-
 # Automatically strip the release zip because this is built in docker locally.
 sed -i 's/<div><a href=release.zip>Download Release ZIP<\/a><\/div>//g' /app/project/dist/index.html
 
